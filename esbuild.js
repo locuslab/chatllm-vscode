8e9fad74652240994ea30edad91e303bca5494f8
Zico Kolter <zkolter@cs.cmu.edu>
2024-01-23 09:55:29 -0500

    trying a different method for including .node files

--- esbuild.js
+++ esbuild.js
@@ -2,6 +2,44 @@ const path = require('path');
 const esbuild = require("esbuild");
 const {sassPlugin} = require("esbuild-sass-plugin");
 
+
+const nativeNodeModulesPlugin = {
+    name: 'native-node-modules',
+    setup(build) {
+      // If a ".node" file is imported within a module in the "file" namespace, resolve 
+      // it to an absolute path and put it into the "node-file" virtual namespace.
+      build.onResolve({ filter: /\.node$/, namespace: 'file' }, args => ({
+        path: require.resolve(args.path, { paths: [args.resolveDir] }),
+        namespace: 'node-file',
+      }));
+  
+      // Files in the "node-file" virtual namespace call "require()" on the
+      // path from esbuild of the ".node" file in the output directory.
+      build.onLoad({ filter: /.*/, namespace: 'node-file' }, args => ({
+        contents: `
+          import path from ${JSON.stringify(args.path)}
+          try { module.exports = require(path) }
+          catch {}
+        `,
+      }));
+  
+      // If a ".node" file is imported within a module in the "node-file" namespace, put
+      // it in the "file" namespace where esbuild's default loading behavior will handle
+      // it. It is already an absolute path since we resolved it to one above.
+      build.onResolve({ filter: /\.node$/, namespace: 'node-file' }, args => ({
+        path: args.path,
+        namespace: 'file',
+      }));
+  
+      // Tell esbuild's default loading behavior to use the "file" loader for
+      // these ".node" files.
+      let opts = build.initialOptions
+      opts.loader = opts.loader || {}
+      opts.loader['.node'] = 'file'
+    },
+  };
+
+
 esbuild.build({
     entryPoints: [path.join(__dirname, 'src/renderer/customMarkdownRenderer.js')],
     bundle: true,
@@ -9,10 +47,6 @@ esbuild.build({
     // minify: true,
     platform: 'browser',
     outfile: path.join(__dirname, 'out/customMarkdownRenderer.js'),
-    loader: {
-        // ensures .node binaries are copied to ./dist
-        ".node": "copy",
-    },
     plugins: [
         sassPlugin({type: "style"})
     ]
@@ -27,11 +61,10 @@ esbuild.build({
     // minify: true,
     platform: 'node',
     outdir: path.join(__dirname, 'out'),
-    loader: {
-        // ensures .node binaries are copied to ./dist
-        ".node": "copy",
-    },
-    external: ['vscode']
+    external: ['vscode'],
+    plugins: [
+        nativeNodeModulesPlugin
+    ]
 }).catch(() => process.exit(1));
 
 
@@ -42,10 +75,6 @@ esbuild.build({
     bundle: true,
     target: 'es2020',
     format: 'esm',
-    loader: {
-        // ensures .node binaries are copied to ./dist
-        ".node": "copy",
-    },
     // minify: true,
     outdir: path.join(__dirname, 'out')
 }).catch(() => process.exit(1));
